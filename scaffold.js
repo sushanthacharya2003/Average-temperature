44ad2f6b962edb765687d4818669f15b4cb8989a
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-11-01 12:15:43 +0530

    starting off

---
+++ scaffold.js
@@ -0,0 +1,14 @@
+// Complete the function calculateAverage
+const weather = [
+  { city: "New York", temperature: 50, pressure: 1015 },
+  { city: "Los Angeles", temperature: 70, pressure: 1010 },
+  { city: "Chicago", temperature: 40, pressure: 1012 },
+  { city: "Houston", temperature: 75, pressure: 1020 },
+  { city: "Miami", temperature: 80, pressure: 1017 },
+];
+const pressureThreshold = 1015;
+function calculateAverage(pressureThreshold) {
+  //Implement your function here
+}
+console.log(calculateAverage(pressureThreshold));
+//Output : 68.3
